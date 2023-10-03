1670e6f9bc186809c3bbfbd60fcfdeeee83124e5
Miguel CS Vanzela <miguelclaudiodasilvavanzela@gmail.com>
2023-10-03 16:39:14 -0300

    Commit inicial com os 5 primeiros desafios

--- ConversorTemperatura/challengeOne.js
+++
@@ -1,8 +0,0 @@
-const grausFarenheit = document.getElementById("faren");
-
-const result = document.getElementById("result");
-grausFarenheit.addEventListener("keydown", () => {
-  const valorConvertidoParaCelcius = (grausFarenheit.value - 32) * 5 / 9;
-  const valorConvertidoParaFarenheit = (grausFarenheit.value * 9/5) + 32;
-  result.innerText = valorConvertidoParaCelcius.toFixed(2);
-});
