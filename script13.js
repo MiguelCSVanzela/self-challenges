8617012309f14da7cf4509609ede0bb459659056
Miguel CS Vanzela <miguelclaudiodasilvavanzela@gmail.com>
2023-10-04 15:26:12 -0300

    Desafio de criar padrões com asterisco

--- script13.js
+++ script13.js
@@ -1,1 +1,24 @@
-// Criador d
+// Criador de padrãos com hash
+
+const qtdHash = document.querySelector("#qtdHash"); 
+const btnCriar = document.querySelector("#criarPadrao"); 
+
+const campoPadrao = document.querySelector("#padrao"); 
+
+const criadorPadrao = (qtd) => {
+    campoPadrao.innerHTML = ""
+    let maxQtd = parseInt(qtd.value); 
+
+    for(let i = maxQtd; i >= 1; i--){
+        let hash = "*"; 
+        let node = document.createElement("li"); 
+        let text = document.createTextNode(hash.repeat(i))
+        node.appendChild(text); 
+        campoPadrao.appendChild(node); 
+    }
+}
+
+
+btnCriar.addEventListener("mouseleave", () => {
+    criadorPadrao(qtdHash); 
+})
